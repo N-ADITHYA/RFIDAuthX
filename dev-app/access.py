c3fcd735c9ea14c4d16eeb19341fcca0e4f45b58
Adithya N <nadithya666@gmail.com>
2025-07-01 20:40:21 +0530

    Added a different logic

--- dev-app/access.py
+++ dev-app/access.py
@@ -15,34 +15,64 @@ def create_user(db: Session, user: User):
     return user_data
 
 
+# def verify_user(db: Session, log: Access_log):
+#     if log.rfid_uid:
+#         db_query = (
+#             db.query(models.User.rfid_uid)
+#             .filter(models.User.rfid_uid==log.rfid_uid)
+#             .first()
+#     )
+#     if not db_query:
+#         raise HTTPException(status_code=404, detail="Rfid UID not found")
+#     user_id = db.query(models.User.id).filter(models.User.rfid_uid == log.rfid_uid).first()
+#     latest_log = (
+#         db.query(models.AccessLog)
+#         .filter(models.AccessLog.user_id == user_id)
+#         .order_by(models.AccessLog.logging_time.desc())
+#         .first()
+#     )
+#     if not latest_log:
+#         is_active = False
+#     if latest_log and latest_log.is_active == True:
+#         is_active = False
+#     elif latest_log and latest_log.is_active == False:
+#         is_active = True
+#     elif not latest_log:
+#         is_active = True
+#
+#     db_log = models.AccessLog(user_id=user_id, rfid_uid=log.rfid_uid, is_active=is_active)
+#     db.add(db_log)
+#     db.commit()
+#     db.refresh(db_log)
+#     return db_log
+
 def verify_user(db: Session, log: Access_log):
-    if log.rfid_uid:
-        db_query = (
-            db.query(models.User.rfid_uid)
-            .filter(models.User.rfid_uid==log.rfid_uid)
-            .first()
-    )
-    if not db_query:
-        raise HTTPException(status_code=404, detail="Rfid UID not found")
-    user_id = db.query(models.User.id).filter(models.User.rfid_uid == log.rfid_uid).first()
+    user = db.query(models.User).filter(models.User.rfid_uid == log.rfid_uid).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="RFID UID not found")
+
+    user_id = user.id
+
     latest_log = (
         db.query(models.AccessLog)
         .filter(models.AccessLog.user_id == user_id)
         .order_by(models.AccessLog.logging_time.desc())
         .first()
     )
-    if not latest_log:
-        is_active = False
-    if latest_log and latest_log.is_active == True:
-        is_active = False
-    elif latest_log and latest_log.is_active == False:
-        is_active = True
-    elif not latest_log:
-        is_active = True
 
-    db_log = models.AccessLog(user_id=user_id, rfid_uid=log.rfid_uid, is_active=is_active)
+    if not latest_log or latest_log.is_active == False:
+        is_active = True   # Logging IN
+    else:
+        is_active = False  # Logging OUT
+
+    db_log = models.AccessLog(
+        user_id=user_id,
+        rfid_uid=log.rfid_uid,
+        is_active=is_active
+    )
     db.add(db_log)
     db.commit()
     db.refresh(db_log)
     return db_log
+
 
